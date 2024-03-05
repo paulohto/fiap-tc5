7802f481d3fca5b4f908b1751d783d5fa621910f
joney.sousa <joneysousa@yahoo.com.br>
2024-03-05 07:56:35 -0300

    Estrutura inicial do projeto

---
+++ tc5-api-products/src/main/java/com/fiap/tc5apiproducts/Tc5ApiProductsApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.tc5apiproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tc5ApiProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tc5ApiProductsApplication.class, args);
+	}
+
+}
